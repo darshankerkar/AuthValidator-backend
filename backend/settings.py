9626aa76b7cf38d3f9ea4c5889199a4d6ab5a041
darshankerkar <darshankerkar09@gmail.com>
2025-09-09 21:31:22 +0530

    Add gunicorn to requirements

--- backend/settings.py
+++ backend/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8x9n_=!k(nqr@zmi!k#8&c3&7y+l6-k=g=lpwj&f9z(xavwp46'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
